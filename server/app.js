dd8c45942b8f34b58ba4d79dffacabb1e22a8a84
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-04-07 20:57:42 +0530

    database created

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv=require("dotenv");
+const connectDB=require("./config/db")
 
+dotenv.config();
+connectDB();
 const app = express();
 
 app.use(cors());
