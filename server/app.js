7f7205583430714f89fe08ad518a68f4cdb322f6
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-04-11 16:41:53 +0530

    app.js updated

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv=require("dotenv");
 const connectDB=require("./config/db")
-
+const authRoutes=require("./routes/authRoutes");
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("DevVault API running");
