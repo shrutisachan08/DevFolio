fdd5cf8fdf3d089b00741632eb90c3ff947615a4
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-04-24 09:48:37 +0530

    new imports are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./pages/Projects.jsx";
 import ProjectDetail from "./pages/ProjectDetail.jsx";
 import AdminLogin from "./pages/admin/Login.jsx";
 import Dashboard from "./pages/admin/Dashboard.jsx";
+import ProjectForm from "./pages/admin/ProjectForm.jsx";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/project/new" element={<ProjectForm />} />
+        <Route path="/admin/project/edit/:id" element={<ProjectForm />} />
       </Routes>
     </AuthProvider>
   );
