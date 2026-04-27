b26a7521d6e401a908f929ea5f9b7fd66d536290
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-04-27 16:31:02 +0530

    remove node_modules from tracking

--- server/app.js
+++ server/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv=require("dotenv");
-const connectDB=require("./config/db")
-const authRoutes=require("./routes/authRoutes");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
+
 dotenv.config();
 connectDB();
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL  // ← Vercel URL after deployment
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
